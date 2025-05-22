70612176f76379dbfaabe991f83af998168b590f
kellykimura <akellykimura007@gmail.com>
2025-05-22 16:44:15 -0300

    ft escalonador

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # ===================== PARTE A =========================
 
@@ -6,6 +7,30 @@ def produto_escalar (mult1, mult2):
 
     produto = sum(mult1 * mult2)
     return produto
+
+def escalonador (A,b):
+    n = len(A)
+
+    for k in range(n-1):
+        for i in range (k+1, n):
+            aux = A[i][k]
+            b[i] = b[i] - b[k] * (aux / A[k][k])
+            for j in range (k,n):
+                A[i][j] = A[i][j] - A[k][j] * (aux / A[k][k])
+    
+    return A,b
+
+def substituicao_regressiva (A,b):
+    n = len(b)
+    x = np.zeros(n)
+
+    for i in reversed(range(n)):
+        soma = 0
+        for j in range (i+1,n):
+            soma += A[i][j] * x[j]
+        x[i] = (b[i] - soma)/A[i][i]
+
+    return x
 
 def polinomio2grau (x,y):
     # APROXIMAÇÃO POLINOMIAL DE GRAU 2
@@ -28,13 +53,17 @@ def polinomio2grau (x,y):
     for i in range(3):
         b[i] = produto_escalar(lista[i], y)
     
-    # decomposição LU para resolver
-    lu_fatorado = lu_factor(A)
-    coeficientes = lu_solve(lu_fatorado, b)
+    print(f"A: {A}")
+    print(f"\n\nb: {b}")
 
+    # resolver o sistema com eliminação de gauss
+    A,b = escalonador(A,b)
+    w = substituicao_regressiva(A,b)
 
+    print(f"A: {A}")
+    print(f"\n\nb: {b}")
 
-    return coeficientes # retorna [a0, a1, a2]
+    return w
 
 
 # importar os dados da tabela para a variável dados
@@ -42,6 +71,19 @@ dados = np.loadtxt('Populacao_PresidentePrudente.dat')
 x = dados[:,0]
 y = dados[:,1]
 
+w = polinomio2grau(x,y)
+print(f"\n\nW: {w}")
 
+plt.scatter(x, y, label='Dados reais')
+plt.xlabel('Ano (A)')
+plt.ylabel('População (P)')
+plt.title('Gráfico de População Presidente Prudente')
+plt.grid(True)
 
+# Curva ajustada
+x_ajuste = np.linspace(min(x), max(x), 200)
+y_ajuste = w[0] + w[1]*x_ajuste + w[2]*x_ajuste**2
+plt.plot(x_ajuste, y_ajuste, color='red', label='Ajuste Polinomial (grau 2)')
+plt.legend()
 
+plt.show()
